f89223e42b55c2bd2948fe4f0b98134d1a37f75f
Wellington <101023510+well97dev@users.noreply.github.com>
2023-02-13 15:02:06 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css'
-import Condicional from './components/Condicional'
+import OutraLista from './components/OutraLista'
 
 function App() {
-
+  const meusItens = ['React', 'Vue', 'Angular', 'Laravel']
 
   return (
     <div className="App">
-      <h1>Rederização Condicional</h1>
-      <Condicional/>
+      <h1>Rederização Listas</h1>
+      <OutraLista itens={meusItens}/>
+      <OutraLista itens={[]}/>
     </div>
   )
 }
--- src/components/Condicional.js
+++ src/components/Condicional.js
@@ -2,17 +2,31 @@ import {useState} from 'react'
 
 function Condicional() {
 
+    const [email, setEmail] = useState()
+    const[userEmail, setUserEmail] = useState()
+
     function enviarEmail(event){
         event.preventDefault()
-        console.log("Testando")
+        setUserEmail(email)
+    }
+
+    function limparEmail(event){
+        setUserEmail('')
     }
 
     return (
         <div>
             <h2>Cadastre e seu e-mail:</h2>
             <form>
-                <input type="email" placeholder="Digite o seu e-mail..."></input>
-                <button type="submit" onClick={enviarEmail}>Enviar-email</button></form>
+                <input type="email" placeholder="Digite o seu e-mail..." onChange={(event) => setEmail(event.target.value)}></input>
+                <button type="submit" onClick={enviarEmail}>Enviar-email</button>
+                {userEmail && (
+                    <div>
+                        <p>o e-mail do usuario é : {userEmail}</p>
+                        <button onClick={limparEmail}>Limpar e-mail</button>
+                    </div>
+                )}
+                </form>
         </div>
     )
 }
